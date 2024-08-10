c632fd935af5e80315fb27e7606f92ebbb94b940
Julian-Timbs <timbsjulian@gmail.com>
2024-08-10 18:56:19 +1000

    added user context

---
+++ src/eventpulse/src/contexts/UserContext.jsx
@@ -0,0 +1,69 @@
+import { createContext, useContext, useState } from "react";
+
+const UserDataContext = createContext(null);
+const UserDispatchContext = createContext(null);
+
+export function useUserData() {
+  return useContext(UserDataContext);
+}
+
+export function useUserDispatch() {
+  return useContext(UserDispatchContext);
+}
+
+export default function UserProvider({ children }) {
+  const [userJwt, setUserJwt] = useState("");
+
+  const [decodedUserJwt, setDecodedUserJwt] = useState({});
+
+  const makeSignupRequest = async (
+    name,
+    email,
+    password,
+    location,
+    preferences,
+  ) => {
+    let bodyData = { name, email, password, location, preferences };
+    console.log("Body data to send is: ");
+    console.log(bodyData);
+    let signUpResult = await fetch("http://localhost:3000/accounts/", {
+      method: "POST",
+      body: JSON.stringify(bodyData),
+      headers: {
+        "content-Type": "application/json",
+      },
+    }).catch((error) => console.error(error));
+
+    signUpResult = await signUpResult.json();
+
+    console.log("Sign up result is: " + JSON.stringify(signUpResult));
+
+    setUserJwt(signUpResult.jwt);
+    setDecodedUserJwt(signUpResult.decodedJwt);
+  };
+
+  const makeLoginRequest = async (email, password) => {
+    let loginResult = await fetch("http://localhost:3000/accounts/auth", {
+      method: "POST",
+      body: { email, password },
+    });
+
+    console.log("Login result is: " + JSON.stringify(loginResult));
+
+    setUserJwt(loginResult.jwt);
+    setDecodedUserJwt(loginResult.decodedJwt);
+  };
+
+  return (
+    <UserDataContext.Provider value={{ userJwt, decodedUserJwt }}>
+      <UserDispatchContext.Provider
+        value={{
+          makeSignupRequest,
+          makeLoginRequest,
+        }}
+      >
+        {children}
+      </UserDispatchContext.Provider>
+    </UserDataContext.Provider>
+  );
+}
