fe5c069bb68c15923f3cd1df0b124a249f9956e3
Julian-Timbs <timbsjulian@gmail.com>
2024-08-10 16:28:52 +1000

    added Material UI and header component

--- src/eventpulse/src/App.jsx
+++ src/eventpulse/src/App.jsx
@@ -5,6 +5,12 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={ <Template /> } >
+        <Route index element={<HomePage />} />
+        <Route path="event" element={<EventPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="account" element={<AccountPage />} />
+        <Route path="eventcreation" element={<EventCreationPage />} />
     </Routes>
 )}
 
